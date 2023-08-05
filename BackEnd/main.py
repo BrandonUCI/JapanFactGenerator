67045e5233d68a4db172df17ba871142282620fe
Brandon Chan <chanbz@uci.edu>
2023-08-04 23:09:49 -0700

    Started Exploring Templates

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -2,10 +2,29 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Brandon Chan',
+        'title': "Blog post 1",
+        'content': "First Post content",
+        'date_posted': "April 20, 2018"
+    },
+    {
+        'author': 'Kenrick Chan',
+        'title': "Blog post 2",
+        'content': "Second Post content",
+        'date_posted': "April 21, 2018"
+    }
+]
+
 @app.route('/')
-def index():
-    return "Hello World"
-    #return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
