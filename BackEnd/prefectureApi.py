83c57457b130ddc64ad2557531bdf761a0ce7dd8
Brandon Chan <chanbz@uci.edu>
2023-08-23 21:35:16 -0700

    Started Modifying PrefectureAPI

--- BackEnd/prefectureApi.py
+++ BackEnd/prefectureApi.py
@@ -1,5 +1,25 @@
 import requests
+import pprint
+url = "https://api.countrystatecity.in/v1/countries/JP/states"
 
-response = requests.get("https://api.countrystatecity.in/v1/countries")
+headers = {
+  'X-CSCAPI-KEY': 'aW4zVHd1WkFuTWcyWURQRnpvTTgxTFU5d05sV3VLdzhhbGxpWEk4YQ=='
+}
 
-print(response.status_code)
+response = requests.request("GET", url, headers=headers)
+data = None
+
+if response.status_code == 200:  # Check if the request was successful
+    data = response.json()  # Convert response content to dictionary
+    for prefecture in data:
+        print(prefecture)
+else:
+    print("Request failed with status code:", response.status_code)
+    
+print(type(data))
+
+
+
+def generateFact(prefecture: str)->str:
+    """Takes in a prefecture and returns a fun fact about the prefecture"""
+    pass
